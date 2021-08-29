be806acb8b038a8849edb31faf4f2849665b7634
Sebastian Manrique <yhoan.manrique@correo.uis.edu.co>
2021-08-29 15:57:25 -0500

    fix: remove unused modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EventService } from './events/event.service';
 import { PrismaService } from './prisma.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { EventsModule } from './events/events.module';
 
 @Module({
   imports: [
@@ -18,10 +15,8 @@ import { EventsModule } from './events/events.module';
       installSubscriptionHandlers: true,
     }),
     AuthModule,
-    UsersModule,
-    EventsModule,
   ],
   controllers: [AppController],
-  providers: [AppService, EventService, PrismaService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
--- src/events/models/category.model.ts
+++
@@ -1,9 +0,0 @@
-import { Field, ID, ObjectType } from '@nestjs/graphql';
-@ObjectType()
-export class Category {
-  @Field((type) => ID)
-  id: string;
-
-  @Field()
-  name: string;
-}
