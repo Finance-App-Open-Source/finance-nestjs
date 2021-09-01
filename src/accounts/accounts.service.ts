9cbd915342e066114732642a51ace58b43c6cbc7
Sebastian Manrique <yhoan.manrique@correo.uis.edu.co>
2021-08-31 22:16:10 -0500

    fix: create account

--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -1,13 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 import { CreateAccountInput } from './models/account.model';
 
 @Injectable()
 export class AccountsService {
+  constructor(private prisma: PrismaService) {}
   async createAccount(createAccountInput: CreateAccountInput, userId: number) {
-    console.log(
-      '🚀 ~ file: accounts.service.ts ~ line 6 ~ AccountsService ~ createAccount ~ payload',
-      createAccountInput,
-      userId,
-    );
+    try {
+      console.log(
+        '🚀 ~ file: accounts.service.ts ~ line 6 ~ AccountsService ~ createAccount ~ payload',
+        createAccountInput,
+        userId,
+      );
+
+      const account = await this.prisma.account.create({
+        data: createAccountInput,
+      });
+      return account;
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
   }
 }
